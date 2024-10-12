037d4f944967368c290efa83ed6b350aaf0f0694
Jay Prakash Pathak <jayprkshpthk@gmail.com>
2024-10-12 19:33:02 +0530

    release workflow

--- s3explorer/forge.config.ts
+++ s3explorer/forge.config.ts
@@ -54,6 +54,18 @@ const config: ForgeConfig = {
             [FuseV1Options.OnlyLoadAppFromAsar]: true,
         }),
     ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'delimitertech',
+                    name: 'electron-starter',
+                },
+                prerelease: true,
+            },
+        },
+    ],
 }
 
 export default config
